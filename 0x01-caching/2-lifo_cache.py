c4b3115b3b09e13eef849c2837662cb9681c28d9
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-20 21:03:34 +0100

    Task 2 - LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ LIFO caching """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
@@ -13,24 +14,21 @@ class LIFOCache(BaseCaching):
             Inits the class
         """
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
             Assigns the item value to the dict for the given key
         """
-        if key is None and item is None:
+        if key is None or item is None:
             return
 
         rslt = self.cache_data
-        if len(rslt) >= BaseCaching.MAX_ITEMS and key not in rslt:
-            k = self.order.pop()
-            del self.cache_data[k]
+        if len(rslt) >= BaseCaching.MAX_ITEMS:
+            k, v = self.cache_data.popitem(True)
             print("DISCARD: {}".format(k))
 
         self.cache_data[key] = item
-        if key not in self.order:
-            self.order.append(key)
 
     def get(self, key):
         """
